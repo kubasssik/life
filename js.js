1a1ee6fc96f222438b6410954fe11c0b63ccf2fe
kubasssik <95367674+kubasssik@users.noreply.github.com>
2022-03-20 15:37:02 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,255 @@
+
+const locality = {
+
+    sity: [
+        {
+            sity: 'Аэропорт Симферополь', km: 0, kmZdSimf: 15, kmZdSevas: 91, kmZdVlad: 129, kmAlup: 117, kmAlusta: 68,
+            kmErmeni: 140, kmBakhic: 45, kmBelog: 57, kmDzhan: 95, kmEvpat: 70,
+        },
+
+        {
+            sity: 'РЖД Симферополь', kmZdSimf: 0, km: 15, kmZdSevas: 81, kmZdVlad: 118, kmAlup: 101, kmAlusta: 53,
+            kmErmeni: 150, kmBakhic: 35, kmBelog: 48, kmDzhan: 95, kmEvpat: 70,
+        },
+
+        {
+            sity: 'РЖД Севастополь', kmZdSevas: 0, km: 90, kmZdSimf: 81, kmZdVlad: 196, kmAlup: 70, kmAlusta: 120,
+            kmErmeni: 228, kmBakhic: 52, kmBelog: 124, kmDzhan: 175, kmEvpat: 117,
+        },
+
+        {
+            sity: 'РЖД Владиславовка', kmZdVlad: 0, km: 130, kmZdSimf: 116, kmZdSevas: 207, kmAlup: 129, kmAlusta: 192,
+            kmErmeni: 192, kmBakhic: 160, kmBelog: 70, kmDzhan: 113, kmEvpat: 185,
+        },
+
+        {
+            sity: 'Алупка г.', kmAlup: 0, km: 117, kmZdSimf: 101, kmZdSevas: 70, kmZdVlad: 129, kmAlusta: 51,
+            kmErmeni: 244, kmBakhic: 91, kmBelog: 142, kmDzhan: 195, kmEvpat: 195,
+        },
+
+        {
+            sity: 'Алушта (ГО)', kmAlusta: 0, km: 68, kmZdSimf: 53, kmZdSevas: 120, kmZdVlad: 145, kmAlup: 51,
+            kmErmeni: 194, kmBakhic: 85, kmBelog: 89, kmDzhan: 144, kmEvpat: 119,
+        },
+
+        {
+            sity: 'Армянск (ГО)', kmErmeni: 0, km: 140, kmZdSimf: 150, kmZdSevas: 228, kmZdVlad: 192, kmAlup: 244, kmAlusta: 194,
+            kmBakhic: 174, kmBelog: 174, kmDzhan: 81, kmEvpat: 132,
+        },
+
+        {
+            sity: 'Бахчисарай г.', kmBakhic: 0, km: 45, kmZdSimf: 35, kmZdSevas: 52, kmZdVlad: 160, kmAlup: 91, kmAlusta: 85,
+            kmErmeni: 174, kmBelog: 90, kmDzhan: 128, kmEvpat: 93,
+        },
+
+        {
+            sity: 'Бахчисарайский район', kmBakhic: 0, km: 45, kmZdSimf: 35, kmZdSevas: 52, kmZdVlad: 160, kmAlup: 91, kmAlusta: 85,
+            kmErmeni: 174, kmBelog: 90, kmDzhan: 128, kmEvpat: 93,
+        },
+
+        {
+            sity: 'Белогорск г.', kmBelog: 0, km: 57, kmZdSimf: 48, kmZdSevas: 124, kmZdVlad: 70, kmAlup: 142, kmAlusta: 89,
+            kmErmeni: 174, kmBakhic: 90, kmDzhan: 98, kmEvpat: 118,
+        },
+
+        {
+            sity: 'Белогорский район', kmBelog: 0, km: 57, kmZdSimf: 50, kmZdSevas: 124, kmZdVlad: 70, kmAlup: 142, kmAlusta: 89,
+            kmErmeni: 174, kmBakhic: 90, kmDzhan: 98, kmEvpat: 118,
+        },
+
+        {
+            sity: 'Джанкой г.', kmDzhan: 0, km: 95, kmZdSimf: 95, kmZdSevas: 175, kmZdVlad: 113, kmAlup: 195, kmAlusta: 144,
+            kmErmeni: 81, kmBakhic: 128, kmBelog: 98, kmEvpat: 130,
+        },
+
+        {
+            sity: 'Джанкойский район', kmDzhan: 0, km: 95, kmZdSimf: 95, kmZdSevas: 175, kmZdVlad: 113, kmAlup: 195, kmAlusta: 144,
+            kmErmeni: 81, kmBakhic: 128, kmBelog: 98, kmEvpat: 130,
+        },
+
+        {
+            sity: 'Евпатория г.', kmEvpat: 0, km: 70, kmZdSimf: 70, kmZdSevas: 117, kmZdVlad: 185, kmAlup: 163, kmAlusta: 119,
+            kmErmeni: 132, kmBakhic: 93, kmBelog: 118, kmDzhan: 130,
+        },
+
+        {
+            sity: 'Керчь г.', km: 217, kmZdSimf: 205, kmZdSevas: 283, kmZdVlad: 99, kmAlup: 285, kmAlusta: 234,
+            kmErmeni: 300, kmBakhic: 249, kmBelog: 164, kmDzhan: 216, kmEvpat: 273,
+        },
+
+        {
+            sity: 'Кировский район', km: 124, kmZdSimf: 113, kmZdSevas: 200, kmZdVlad: 20, kmAlup: 200, kmAlusta: 145,
+            kmErmeni: 182, kmBakhic: 153, kmBelog: 72, kmDzhan: 104, kmEvpat: 173,
+        },
+
+        {
+            sity: 'Красногвардейский район', km: 70, kmZdSimf: 70, kmZdSevas: 148, kmZdVlad: 108, kmAlup: 168, kmAlusta: 118,
+            kmErmeni: 100, kmBakhic: 100, kmBelog: 80, kmDzhan: 30, kmEvpat: 103,
+        },
+
+        {
+            sity: 'Красноперекопск г.', km: 120, kmZdSimf: 122, kmZdSevas: 202, kmZdVlad: 174, kmAlup: 225, kmAlusta: 174,
+            kmErmeni: 22, kmBakhic: 153, kmBelog: 157, kmDzhan: 61, kmEvpat: 114,
+        },
+
+        {
+            sity: 'Красноперекопский район', km: 135, kmZdSimf: 122, kmZdSevas: 202, kmZdVlad: 174, kmAlup: 225, kmAlusta: 174,
+            kmErmeni: 22, kmBakhic: 153, kmBelog: 157, kmDzhan: 61, kmEvpat: 114,
+        },
+
+        {
+            sity: 'Ленинский район', km: 165, kmZdSimf: 154, kmZdSevas: 233, kmZdVlad: 50, kmAlup: 235, kmAlusta: 183,
+            kmErmeni: 245, kmBakhic: 199, kmBelog: 113, kmDzhan: 159, kmEvpat: 227,
+        },
+
+        {
+            sity: 'Нижнегорский район', km: 105, kmZdSimf: 95, kmZdSevas: 189, kmZdVlad: 73, kmAlup: 190, kmAlusta: 139,
+            kmErmeni: 123, kmBakhic: 137, kmBelog: 51, kmDzhan: 46, kmEvpat: 142,
+        },
+
+        {
+            sity: 'Николаевка пгт', km: 50, kmZdSimf: 42, kmZdSevas: 67, kmZdVlad: 157, kmAlup: 117, kmAlusta: 96,
+            kmErmeni: 163, kmBakhic: 43, kmBelog: 89, kmDzhan: 130, kmEvpat: 54,
+        },
+
+        {
+            sity: 'Новофедоровка пгт', km: 41, kmZdSimf: 55, kmZdSevas: 93, kmZdVlad: 169, kmAlup: 143, kmAlusta: 108,
+            kmErmeni: 142, kmBakhic: 73, kmBelog: 97, kmDzhan: 122, kmEvpat: 32,
+        },
+
+        {
+            sity: 'Первомайский район', km: 95, kmZdSimf: 95, kmZdSevas: 176, kmZdVlad: 157, kmAlup: 197, kmAlusta: 148,
+            kmErmeni: 51, kmBakhic: 128, kmBelog: 134, kmDzhan: 51, kmEvpat: 88,
+        },
+
+        {
+            sity: 'Раздольненский район', km: 118, kmZdSimf: 122, kmZdSevas: 201, kmZdVlad: 193, kmAlup: 224, kmAlusta: 175,
+            kmErmeni: 62, kmBakhic: 153, kmBelog: 160, kmDzhan: 81, kmEvpat: 74,
+        },
+
+        {
+            sity: 'Саки г.', km: 50, kmZdSimf: 50, kmZdSevas: 92, kmZdVlad: 160, kmAlup: 141, kmAlusta: 98,
+            kmErmeni: 133, kmBakhic: 71, kmBelog: 94, kmDzhan: 120, kmEvpat: 23,
+        },
+
+        {
+            sity: 'Сакский район', km: 50, kmZdSimf: 50, kmZdSevas: 92, kmZdVlad: 160, kmAlup: 141, kmAlusta: 98,
+            kmErmeni: 133, kmBakhic: 71, kmBelog: 94, kmDzhan: 120, kmEvpat: 23,
+        },
+
+        {
+            sity: "Севастополь (ГФЗ)", km: 90, kmZdSimf: 81, kmZdSevas: 0, kmZdVlad: 196, kmAlup: 70, kmAlusta: 120,
+            kmErmeni: 222, kmBakhic: 52, kmBelog: 124, kmDzhan: 175, kmEvpat: 117,
+        },
+
+        {
+            sity: 'Симферополь г.', km: 15, kmZdSimf: 0, kmZdSevas: 81, kmZdVlad: 118, kmAlup: 101, kmAlusta: 53,
+            kmErmeni: 150, kmBakhic: 35, kmBelog: 48, kmDzhan: 95, kmEvpat: 70,
+        },
+
+        {
+            sity: 'Симферопольский район', km: 15, kmZdSimf: 0, kmZdSevas: 81, kmZdVlad: 118, kmAlup: 101, kmAlusta: 53,
+            kmErmeni: 150, kmBakhic: 35, kmBelog: 48, kmDzhan: 95, kmEvpat: 70,
+        },
+
+        {
+            sity: 'Советский район', km: 114, kmZdSimf: 103, kmZdSevas: 178, kmZdVlad: 50, kmAlup: 193, kmAlusta: 144,
+            kmErmeni: 147, kmBakhic: 145, kmBelog: 66, kmDzhan: 70, kmEvpat: 162,
+        },
+        {
+            sity: 'Судак (ГО)', km: 115, kmZdSimf: 105, kmZdSevas: 195,
+            kmZdVlad: 68,//надо подумать
+            kmAlup: 131, kmAlusta: 80,
+            kmErmeni: 231, kmBakhic: 148, kmBelog: 72, kmDzhan: 145, kmEvpat: 173,
+        },
+        {
+            sity: 'Феодосия (ГО)', km: 125, kmZdSimf: 115, kmZdSevas: 211, kmZdVlad: 15,//обдумать 
+            kmAlup: 187,
+            kmErmeni: 209, kmBakhic: 162, kmBelog: 82, kmDzhan: 128, kmEvpat: 184,
+        },
+        {
+            sity: 'Черноморский район', km: 145, kmZdSimf: 145, kmZdSevas: 188, kmZdVlad: 258, kmAlup: 238, kmAlusta: 194,
+            kmErmeni: 140, kmBakhic: 167, kmBelog: 189, kmDzhan: 160, kmEvpat: 75,
+        },
+
+        {
+            sity: 'Щёлкино г.', km: 185, kmZdSimf: 171, kmZdSevas: 267, kmZdVlad: 65, kmAlup: 251, kmAlusta: 200,
+            kmErmeni: 263, kmBelog: 139, kmDzhan: 177, kmEvpat: 240,
+        },
+
+        {
+            sity: 'Ялта (ГО)', km: 100, kmZdSimf: 85, kmZdSevas: 90, kmZdVlad: 180, kmAlup: 17, kmAlusta: 36,
+            kmErmeni: 224, kmBelog: 120, kmDzhan: 177, kmEvpat: 160,
+        },
+        { sity: 'Другой...', },
+    ]
+}
+
+let itemUl = document.querySelector('ol')
+
+
+let numLi= [];
+//создаем список длинной locality
+const fragment = document.createDocumentFragment();
+
+for( let i = 0; i < locality.sity.length; i++){
+    numLi.push(locality.sity[i].sity) 
+    //console.log(numLi); 
+}
+for( let i = 0; i < numLi.length; i++){
+    const newLi = document.createElement('li');
+    newLi.textContent =  numLi[i]; 
+    newLi.addEventListener('click', ()=>{
+       
+       document.querySelector('.input').value = newLi.textContent;
+    }) 
+    fragment.appendChild(newLi) 
+
+}
+itemUl.appendChild(fragment)
+
+
+
+//Скрываем список
+itemUl.classList.add('hide')
+let itemLi = document.querySelectorAll('li');
+itemLi.forEach(e => {
+    e.classList.add('hide')
+});
+
+
+//живой поиск
+document.querySelector('.input').oninput = function () {
+    let val = this.value.trim()
+    if (val == '') {
+        itemUl.classList.add('hide')
+
+        itemLi.forEach(e => {
+            e.classList.add('hide');
+        });
+    }
+    if (val != '') {
+        itemUl.classList.remove('hide')
+        itemLi.forEach(e => {
+            if (e.textContent.search(RegExp(val, "gi")) == -1) {
+                e.classList.add('hide');
+                e.textContent = e.textContent;
+            }
+            else {
+                e.classList.remove('hide');
+                let str = e.textContent;
+                e.innerHTML = insertMark(str, e.textContent.search(RegExp(val, "gi")), val.length)
+            }
+        })
+    }
+}
+function insertMark(string, pos, len) {
+    return `${string.slice(0, pos)}<mark>${string.slice(pos, pos + len)}</mark>${string.slice(pos + len)}`;
+}
+
+
+
+
+
+        //console.log(this.value)
+
